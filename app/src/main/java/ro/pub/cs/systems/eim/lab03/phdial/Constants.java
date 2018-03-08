17eda31b23ab6bc7fd5dfd6fa6b10f6607b61fc4
dragos-niculescu <dragos-niculescu@users.noreply.github.com>
2018-03-08 11:59:20 +0200

    lab3 p1-p5

---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/phdial/Constants.java
@@ -0,0 +1,10 @@
+package ro.pub.cs.systems.eim.lab03.phdial;
+import ro.pub.cs.systems.eim.lab03.phdial.R;
+
+/**
+ * Created by dniculescu on 3/6/18.
+ */
+
+public interface Constants {
+    final public static String TAG = "phdialer";
+}
